e56426a218867a7670f8f1da23b6a588d7ff2adf
Kevin Pramudya <if1972053@student.it.maranatha.edu>
2023-01-13 20:54:12 +0700

    Login Succeed

--- MovieApp/app/src/main/java/com/kevin/uasmovieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/kevin/uasmovieapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.kevin.uasmovieapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.kevin.uasmovieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val email = intent.getStringExtra("email")
+
+        binding.tvEmail.text = email
+
+        binding.btnSignoutGoogle.setOnClickListener {
+            auth.signOut()
+            googleSignInClient.signOut()
+            startActivity(Intent(this, AuthGoogleActivity::class.java))
+        }
     }
 }

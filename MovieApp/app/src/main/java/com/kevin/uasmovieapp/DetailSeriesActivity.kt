6041c6feb2f4d6a3088f80f1c1d2ee04343106b7
Kevin Pramudya <if1972053@student.it.maranatha.edu>
2023-01-14 11:04:25 +0700

    Add Adapter

---
+++ MovieApp/app/src/main/java/com/kevin/uasmovieapp/DetailSeriesActivity.kt
@@ -0,0 +1,11 @@
+package com.kevin.uasmovieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailSeriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_series)
+    }
+}
---
+++ MovieApp/app/src/main/java/com/kevin/uasmovieapp/SeriesAdapter.kt
@@ -0,0 +1,4 @@
+package com.kevin.uasmovieapp
+
+class SeriesAdapter {
+}
